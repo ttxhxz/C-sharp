623cdb36b5d09e3b63d845bd4fb103c168ac9e8b
LoveSSJ <liu.wen.guang@qq.com>
2019-02-02 23:55:02 +0800

    2019.2.2 15播放器项目 File类和Path类

---
+++ 15-播放器项目/01-Path类复习/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Path类复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\User\1.txt";
+        }
+    }
+}
---
+++ 15-播放器项目/02-File类/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_File类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //操作文件的
+            //剪切 复制 创建 移除
+
+            //File.Create(@"C:\Users\liuwe\Desktop\小熊猫.txt");
+            //Console.WriteLine("创建成功");
+
+
+            //File.Delete(@"C:\Users\liuwe\Desktop\小熊猫.txt");
+            //Console.WriteLine("删除成功");
+
+            byte[] buffer = File.ReadAllBytes(@"C:\Users\liuwe\Desktop\1.txt");
+            string str = Encoding.UTF8.GetString(buffer,0,buffer.Length);
+            Console.WriteLine(str);
+            Console.WriteLine(str.Length);
+
+            Console.ReadKey();
+
+        }
+    }
+}
